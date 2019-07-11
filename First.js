7192a2d0666c639fe2c37c47091af73c2c066127
kutec <ku@kutec.co.in>
2019-07-11 13:25:06 +0530

    Initial commit

---
+++ First.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+console.log(React);
+
+export default ({ name }) => <h1>KUSHAL {name}!</h1>;
---
+++ index.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import Abc from './Hello';
+import KU from './First';
+import './style.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: 'React'
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <Abc name={this.state.name} />
+        <KU name={this.state.name} />
+        <p>
+          Start editing to see some magic happen :)
+        </p>
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById('root'));
